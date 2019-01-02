4c9f4de58dc5ed3fbe56f63d6ec3709ff11caada
Nate Lust <nlust@astro.princeton.edu>
2019-01-02 10:57:21 -0500

    Upload experimental docs

---
+++ doc/reups - Rust_files/search-index.js
@@ -0,0 +1,3 @@
+var N = null;var searchIndex = {};
+searchIndex["reups"]={"doc":"A reimagined, from scratch, reimplementation of the eups environment modules system in rust.","items":[],"paths":[]};
+initSearch(searchIndex);
