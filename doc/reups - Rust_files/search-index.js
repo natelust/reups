db9770bb4d06375f9d22a396ef12332a325802b5
Nate Lust <nlust@astro.princeton.edu>
2019-01-02 10:58:32 -0500

    Remove experimental docs

--- doc/reups - Rust_files/search-index.js
+++
@@ -1,3 +0,0 @@
-var N = null;var searchIndex = {};
-searchIndex["reups"]={"doc":"A reimagined, from scratch, reimplementation of the eups environment modules system in rust.","items":[],"paths":[]};
-initSearch(searchIndex);
