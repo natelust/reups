47e055e378f2e845724bd0a6dd7e2b382456ec51
Nate Lust <nlust@astro.princeton.edu>
2019-02-15 09:28:18 -0600

    Handle env save/restore args better

--- src/prep.rs
+++ src/prep.rs
@@ -20,11 +20,11 @@ pub fn build_prep_string() -> &'static str {
 };
 
 rrestore() {
-eval $(reups env restore \"$*\");
+eval $(reups env restore \"$@\");
 };
 
 rsave() {
-reups env save \"$*\";
+reups env save \"$@\";
 };
 "
 }
