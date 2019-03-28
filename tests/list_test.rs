b5907481ea8a94639ba583d9d82435297c702743
Nate Lust <nlust@astro.princeton.edu>
2019-03-28 10:51:16 -0500

    Begin adding integration tests

---
+++ tests/list_test.rs
@@ -0,0 +1,77 @@
+extern crate reups_lib;
+use reups_lib as reups;
+use std::io::Cursor;
+use std::path::PathBuf;
+
+fn common(args: Vec<&str>, expected: &str) {
+    let mut args: Vec<String> = args.iter().map(|&s| s.to_string()).collect();
+    let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    root.push("resources/posix_db");
+    let root_string = root.to_str().unwrap().to_string();
+    args.push(root_string);
+
+    let mut cursor = Cursor::new(vec![]);
+
+    let app = reups::build_cli();
+    let matches = app.get_matches_from(args);
+    let (_, m) = matches.subcommand();
+    let result = reups::list_command(m.unwrap(), &matches, &mut cursor);
+    assert!(result.is_ok());
+
+    let output = String::from_utf8(cursor.into_inner()).unwrap();
+    assert_eq!(output, expected);
+}
+
+#[test]
+fn test_list_full() {
+    let args = vec!["reups", "list", "-U", "-S", "-Z"];
+    let expected = "fooA                                            v1          []
+
+fooA                                            v2          []
+
+fooA                                            v3          [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooB                                            v1          [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooC                                            v1          []
+
+fooC                                            v2          [\u{1b}[96mcurrent\u{1b}[0m]
+";
+    common(args, expected)
+}
+
+#[test]
+fn test_list_only_vers() {
+    let args = vec!["reups", "list", "-U", "-S", "--onlyVers", "-Z"];
+    let expected = "fooA                               [v3, v1, v2]
+
+fooB                               [v1]
+
+fooC                               [v1, v2]
+";
+    common(args, expected)
+}
+
+#[test]
+fn test_list_only_tags() {
+    let args = vec!["reups", "list", "-U", "-S", "--onlyTags", "-Z"];
+    let expected = "fooA                               [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooB                               [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooC                               [\u{1b}[96mcurrent\u{1b}[0m]
+";
+    common(args, expected)
+}
+
+#[test]
+fn test_list_one_product() {
+    let args = vec!["reups", "list", "-U", "-S", "fooA", "-Z"];
+    let expected = "fooA                                            v1          []
+
+fooA                                            v2          []
+
+fooA                                            v3          [\u{1b}[96mcurrent\u{1b}[0m]
+";
+    common(args, expected)
+}
---
+++ tests/setup_test.rs
@@ -0,0 +1,46 @@
+extern crate reups_lib;
+use reups_lib as reups;
+use std::env;
+use std::io::Cursor;
+use std::path::PathBuf;
+
+fn common(args: Vec<&str>, expected: &str) {
+    let current_exe = std::env::current_exe().unwrap();
+    let exe_string = current_exe.to_str().unwrap().to_string();
+    env::set_var("PATH", "");
+    env::set_var("REUPS_HISTORY", "");
+    let mut args: Vec<String> = args.iter().map(|&s| s.to_string()).collect();
+    let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let crate_path = root.clone();
+    root.push("resources/posix_db");
+    let root_string = root.to_str().unwrap().to_string();
+    args.push(root_string);
+
+    let mut cursor = Cursor::new(vec![]);
+
+    let app = reups::build_cli();
+    let matches = app.get_matches_from(args);
+    let (_, m) = matches.subcommand();
+    let result = reups::setup_command(m.unwrap(), &matches, &mut cursor);
+    assert!(result.is_ok());
+
+    let mut output = String::from_utf8(cursor.into_inner()).unwrap();
+    output = output.replace(" --nocapture", "");
+    let expected = expected.replace("$CRATE_PATH", crate_path.to_str().unwrap());
+    let expected = expected.replace("$CURRENT_EXE", exe_string.as_str());
+    assert_eq!(output, expected);
+}
+
+#[test]
+fn test_setup_exact() {
+    let args = vec!["reups", "setup", "-U", "-S", "fooA", "-Z"];
+    let expected = "export FOOB_DIR=$CRATE_PATH/resources/test_packages/fooB SETUP_FOOA=fooA\\ v3\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ REUPS_HISTORY=\"|$CURRENT_EXE\" PATH=$CRATE_PATH/resources/test_packages/fooA/bin: SETUP_FOOB=fooB\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOC_DIR=$CRATE_PATH/resources/test_packages/fooC SETUP_FOOC=fooC\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOA_DIR=$CRATE_PATH/resources/test_packages/fooA \n";
+    common(args, expected);
+}
+
+#[test]
+fn test_setup_inexact() {
+    let args = vec!["reups", "setup", "-U", "-S", "-E", "fooA", "-Z"];
+    let expected = "export FOOB_DIR=$CRATE_PATH/resources/test_packages/fooB SETUP_FOOA=fooA\\ v3\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ REUPS_HISTORY=\"|$CURRENT_EXE\" PATH=$CRATE_PATH/resources/test_packages/fooA/bin: SETUP_FOOB=fooB\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOC_DIR=$CRATE_PATH/resources/test_packages/fooC SETUP_FOOC=fooC\\ v2\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOA_DIR=$CRATE_PATH/resources/test_packages/fooA \n";
+    common(args, expected);
+}
