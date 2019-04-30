67fed7e490ab35ced55e0acf185e6ea3bb93ec8b
Nate Lust <nlust@astro.princeton.edu>
2019-04-30 15:08:54 -0400

    Add tests that use JSON DB source

--- tests/list_test.rs
+++ tests/list_test.rs
@@ -3,10 +3,14 @@ use reups_lib as reups;
 use std::io::Cursor;
 use std::path::PathBuf;
 
-fn common(args: Vec<&str>, expected: &str) {
+fn common(args: Vec<&str>, expected: &str, json: bool) {
     let mut args: Vec<String> = args.iter().map(|&s| s.to_string()).collect();
     let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    root.push("resources/posix_db");
+    if json {
+        root.push("resources/json_db/json_db.json")
+    } else {
+        root.push("resources/posix_db");
+    }
     let root_string = root.to_str().unwrap().to_string();
     args.push(root_string);
 
@@ -37,7 +41,25 @@ fooC                                            v1          []
 
 fooC                                            v2          [\u{1b}[96mcurrent\u{1b}[0m]
 ";
-    common(args, expected)
+    common(args, expected, false)
+}
+
+#[test]
+fn test_list_full_json() {
+    let args = vec!["reups", "list", "-U", "-S", "-Z"];
+    let expected = "fooA                                            v1          []
+
+fooA                                            v2          []
+
+fooA                                            v3          [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooB                                            v1          [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooC                                            v1          []
+
+fooC                                            v2          [\u{1b}[96mcurrent\u{1b}[0m]
+";
+    common(args, expected, true)
 }
 
 #[test]
@@ -49,7 +71,19 @@ fooB                               [v1]
 
 fooC                               [v1, v2]
 ";
-    common(args, expected)
+    common(args, expected, false)
+}
+
+#[test]
+fn test_list_only_vers_json() {
+    let args = vec!["reups", "list", "-U", "-S", "--onlyVers", "-Z"];
+    let expected = "fooA                               [v3, v1, v2]
+
+fooB                               [v1]
+
+fooC                               [v1, v2]
+";
+    common(args, expected, true)
 }
 
 #[test]
@@ -61,7 +95,19 @@ fooB                               [\u{1b}[96mcurrent\u{1b}[0m]
 
 fooC                               [\u{1b}[96mcurrent\u{1b}[0m]
 ";
-    common(args, expected)
+    common(args, expected, false)
+}
+
+#[test]
+fn test_list_only_tags_json() {
+    let args = vec!["reups", "list", "-U", "-S", "--onlyTags", "-Z"];
+    let expected = "fooA                               [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooB                               [\u{1b}[96mcurrent\u{1b}[0m]
+
+fooC                               [\u{1b}[96mcurrent\u{1b}[0m]
+";
+    common(args, expected, true)
 }
 
 #[test]
@@ -73,5 +119,17 @@ fooA                                            v2          []
 
 fooA                                            v3          [\u{1b}[96mcurrent\u{1b}[0m]
 ";
-    common(args, expected)
+    common(args, expected, false)
+}
+
+#[test]
+fn test_list_one_product_json() {
+    let args = vec!["reups", "list", "-U", "-S", "fooA", "-Z"];
+    let expected = "fooA                                            v1          []
+
+fooA                                            v2          []
+
+fooA                                            v3          [\u{1b}[96mcurrent\u{1b}[0m]
+";
+    common(args, expected, true)
 }
--- tests/setup_test.rs
+++ tests/setup_test.rs
@@ -4,7 +4,7 @@ use std::env;
 use std::io::Cursor;
 use std::path::PathBuf;
 
-fn common(args: Vec<&str>, expected: &str) {
+fn common(args: Vec<&str>, expected: &str, json: bool) {
     let current_exe = std::env::current_exe().unwrap();
     let exe_string = current_exe.to_str().unwrap().to_string();
     env::set_var("PATH", "");
@@ -12,7 +12,11 @@ fn common(args: Vec<&str>, expected: &str) {
     let mut args: Vec<String> = args.iter().map(|&s| s.to_string()).collect();
     let mut root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let crate_path = root.clone();
-    root.push("resources/posix_db");
+    if !json {
+        root.push("resources/posix_db");
+    } else {
+        root.push("resources/json_db/json_db.json");
+    }
     let root_string = root.to_str().unwrap().to_string();
     args.push(root_string);
 
@@ -35,12 +39,26 @@ fn common(args: Vec<&str>, expected: &str) {
 fn test_setup_exact() {
     let args = vec!["reups", "setup", "-U", "-S", "fooA", "-Z"];
     let expected = "export FOOB_DIR=$CRATE_PATH/resources/test_packages/fooB SETUP_FOOA=fooA\\ v3\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ REUPS_HISTORY=\"|$CURRENT_EXE\" PATH=$CRATE_PATH/resources/test_packages/fooA/bin: SETUP_FOOB=fooB\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOC_DIR=$CRATE_PATH/resources/test_packages/fooC SETUP_FOOC=fooC\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOA_DIR=$CRATE_PATH/resources/test_packages/fooA \n";
-    common(args, expected);
+    common(args, expected, false);
 }
 
 #[test]
 fn test_setup_inexact() {
     let args = vec!["reups", "setup", "-U", "-S", "-E", "fooA", "-Z"];
     let expected = "export FOOB_DIR=$CRATE_PATH/resources/test_packages/fooB SETUP_FOOA=fooA\\ v3\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ REUPS_HISTORY=\"|$CURRENT_EXE\" PATH=$CRATE_PATH/resources/test_packages/fooA/bin: SETUP_FOOB=fooB\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOC_DIR=$CRATE_PATH/resources/test_packages/fooC SETUP_FOOC=fooC\\ v2\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/posix_db/ FOOA_DIR=$CRATE_PATH/resources/test_packages/fooA \n";
-    common(args, expected);
+    common(args, expected, false);
+}
+
+#[test]
+fn test_setup_exact_json() {
+    let args = vec!["reups", "setup", "-U", "-S", "fooA", "-Z"];
+    let expected = "export FOOB_DIR=$CRATE_PATH/resources/test_packages/fooB SETUP_FOOA=fooA\\ v3\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/json_db/json_db.json REUPS_HISTORY=\"|$CURRENT_EXE\" PATH=$CRATE_PATH/resources/test_packages/fooA/bin: SETUP_FOOB=fooB\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/json_db/json_db.json FOOC_DIR=$CRATE_PATH/resources/test_packages/fooC SETUP_FOOC=fooC\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/json_db/json_db.json FOOA_DIR=$CRATE_PATH/resources/test_packages/fooA \n";
+    common(args, expected, true);
+}
+
+#[test]
+fn test_setup_inexact_json() {
+    let args = vec!["reups", "setup", "-U", "-S", "-E", "fooA", "-Z"];
+    let expected = "export FOOB_DIR=$CRATE_PATH/resources/test_packages/fooB SETUP_FOOA=fooA\\ v3\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/json_db/json_db.json REUPS_HISTORY=\"|$CURRENT_EXE\" PATH=$CRATE_PATH/resources/test_packages/fooA/bin: SETUP_FOOB=fooB\\ v1\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/json_db/json_db.json FOOC_DIR=$CRATE_PATH/resources/test_packages/fooC SETUP_FOOC=fooC\\ v2\\ -f\\ Linux64\\ -Z\\ $CRATE_PATH/resources/json_db/json_db.json FOOA_DIR=$CRATE_PATH/resources/test_packages/fooA \n";
+    common(args, expected, true);
 }
